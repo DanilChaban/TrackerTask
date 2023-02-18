0e2aaae60143d6000345e56cd3b14f2dd62d7d50
Danil Chaban <krigerblood@gmail.com>
2023-02-18 14:15:55 +0200

    refactor validators and add guard with takeUntil RxJs Operator

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {Observable, of} from 'rxjs';
+import {AuthService} from "../services/auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private readonly authService: AuthService,
+              private readonly router: Router) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    if (this.authService.isAuthenticatedForGuard()) {
+      return of(true);
+    } else {
+      this.router.navigate(['']);
+    }
+
+    return of(false);
+  }
+}
